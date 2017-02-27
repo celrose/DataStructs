cc53b4542cb0de9ec2be6b8a176eb99e4027ff0e
Rocelle <annecadavos@yahoo.com>
2017-02-27 13:45:37 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def front(self):
+        pass
+
+    def rear(self):
+        pass
+
+    def size(self):
+        pass
